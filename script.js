9d41afb82e87a7b46e079fb0b68539bca01c0ec1
Lee Marvin <ryenem8@gmail.com>
2020-09-25 16:44:37 -0700

    formatted HTML and added timer to the top

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var timeTime = moment().format("MMM Do YYYY");
+
+$("#currentDay").append(timeTime);
+
+$(document).ready(function() {
+$(".saveBtn").on("click", function() {
+    alert("I've been clicked!");
+ });
+
+
+
+
+
+
+
+
+
+})
